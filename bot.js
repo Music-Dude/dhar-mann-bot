53eaf2f25c747e0316940239f86773259b6b2c0b
Music-Dude <tvach2167@gmail.com>
2021-09-07 18:10:06 -0400

    Use 4 spaces for indentation

--- bot.js
+++ bot.js
@@ -5,16 +5,16 @@ const { people, actions, endings } = require('./words.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', async msg => {
-  if (msg.content.toLowerCase().includes('dhar man')) {
-    console.log(`Responded to ${msg.author.username}#${msg.author.discriminator}`);
-    const title = `${people[Math.floor(Math.random() * people.length)]} ${actions[Math.floor(Math.random() * actions.length)]} ${people[Math.floor(Math.random() * people.length)]}, ${endings[Math.floor(Math.random() * endings.length)]}!`;
-    msg.reply(title)
-      .catch(console.error);
-  }
+    if (msg.content.toLowerCase().includes('dhar man')) {
+        console.log(`Responded to ${msg.author.username}#${msg.author.discriminator}`);
+        const title = `${people[Math.floor(Math.random() * people.length)]} ${actions[Math.floor(Math.random() * actions.length)]} ${people[Math.floor(Math.random() * people.length)]}, ${endings[Math.floor(Math.random() * endings.length)]}!`;
+        msg.reply(title)
+            .catch(console.error);
+    }
 });
 
 client.login(token);
