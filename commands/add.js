14cf25fc9e3825d795b0586210bad5067de4fdbb
Music-Dude <Music-Dude@users.noreply.github.com>
2021-09-10 20:53:00 +0000

    Update permissions integer

--- commands/add.js
+++ commands/add.js
@@ -1,7 +1,7 @@
 function add(client, msg, args) {
     let embed = {
         title: 'Add Dhar Mann to your server',
-        description: `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=3071`,
+        description: `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=274877959232`,
         color: Math.floor(Math.random() * 16777215)
     };
 
