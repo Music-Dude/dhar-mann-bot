9b0813aaac40b73e837bfbe740c42bf334c3c31b
Music-Dude <Music-Dude@users.noreply.github.com>
2021-09-12 15:49:51 +0000

    Add button to add command

--- commands/add.js
+++ commands/add.js
@@ -1,13 +1,26 @@
 /* globals client */
 
 function add(msg) {
+    let url = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=274877959232`;
     let embed = {
         title: 'Add Dhar Mann to your server',
-        description: `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=274877959232`,
+        description: url,
         color: Math.floor(Math.random() * 16777215)
     };
 
-    msg.reply({ embeds: [embed]});
+    let button = {
+        type: 1,
+        components: [
+            {
+                label: "Add Dhar Mann to your server!",
+                type: 2,
+                style: 5,
+                url: url
+            }
+        ]
+    };
+
+    msg.reply({ embeds: [embed], components: [button] });
 }
 
 module.exports = add;
